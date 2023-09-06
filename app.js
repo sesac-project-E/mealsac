1fa8573bf0d50ec4f46edd02c940005aabec3b1d
Cho Gain <chogain05@gmail.com>
2023-09-06 18:28:51 +0900

    init: 프로젝트 초기 생성

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const session = require('express-session');
+dotenv.config();
+
+const app = express();
+const db = require('./models');
+const PORT = process.env.PORT;
+// const PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.set('/views', 'view');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
