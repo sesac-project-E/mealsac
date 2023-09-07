1064cc476a16b8bb9d8c7eacf4e6102f613e60fa
jjsh03 <aberrant03@gmail.com>
2023-09-07 23:19:29 +0900

    #3 feat: 아이디/닉네임 공백 검사

--- static/js/register.js
+++ static/js/register.js
@@ -1,23 +1,28 @@
-function overlapId() {
-  // db userId와 중복 확인
-}
-
-function doRegister() {
+const doRegister = () => {
+  const inputId = document.getElementById('inputId').value;
+  const inputName = document.getElementById('inputName').value;
   const inputPw = document.getElementById('inputPw').value;
   const confirmPw = document.getElementById('confirmPw').value;
   const pwPattern = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/;
   const pwMsg = document.querySelector('#pwMsg');
-  if (!pwPattern.test(inputPw)) {
-    pwMsg.textContent = '비밀번호는 8자 이상 영문 + 숫자를 혼합하여야 합니다.';
-  } else if (inputPw !== confirmPw) {
-    pwMsg.textContent = '비밀번호가 일치하지 않습니다.';
+  const idMsg = document.querySelector('#idMsg');
+  const nameMsg = document.querySelector('#nameMsg');
+  if (inputId === '') {
+    idMsg.textContent = '아이디를 입력해주세요';
+  } else if (inputPw === '') {
+    pwMsg.textContent = '비밀번호를 입력해주세요';
+  } else if (inputName === '') {
+    nameMsg.textContent = '닉네임을 입력해주세요';
   } else {
-    // db로 보내는 코드 필요
-    alert('회원가입 성공!');
-    document.location.href = '/login';
+    if (!pwPattern.test(inputPw)) {
+      pwMsg.textContent =
+        '비밀번호는 8자 이상 영문 + 숫자를 혼합하여야 합니다.';
+    } else if (inputPw !== confirmPw) {
+      pwMsg.textContent = '비밀번호가 일치하지 않습니다.';
+    } else {
+      // db로 보내는 코드 필요
+      alert('회원가입 성공!');
+      document.location.href = '/login';
+    }
   }
-}
-
-function overlapName() {
-  // db userName와 중복 확인
-}
+};
