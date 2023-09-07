2f7e212cc10a69fe9885d01e3cce99c8a9898ddb
jjsh03 <aberrant03@gmail.com>
2023-09-07 20:58:06 +0900

    #3 init: 초기생성, static 연결

--- app.js
+++ app.js
@@ -10,10 +10,15 @@ const PORT = process.env.PORT;
 app.set('view engine', 'ejs');
 app.set('/views', 'view');
 app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static(__dirname + '/static'));
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register/index');
 });
 
 app.listen(PORT, () => {
