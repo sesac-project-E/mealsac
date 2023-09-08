30ab6f23ffaa41fa623c64593914e172d152cf7d
kdm111 <pjsu94@naver.com>
2023-09-08 16:45:21 +0900

    feat : restaurant 모델 컨트롤러 라우터 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const restaurantRouter = require("./restaurantRouter.js")
+
+router.use("/restaurant", restaurantRouter)
 
 module.exports = router;
