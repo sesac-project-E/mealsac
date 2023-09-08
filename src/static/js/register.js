57710c76a4f2e301007d9bc928f7c705aaebe774
jjsh03 <aberrant03@gmail.com>
2023-09-08 19:06:03 +0900

    #3 feat: Register 중복확인, 비밀번호 유효성 검사

--- src/static/js/register.js
+++ src/static/js/register.js
@@ -1,28 +1,63 @@
-const doRegister = () => {
-  const inputId = document.getElementById('inputId').value;
-  const inputName = document.getElementById('inputName').value;
-  const inputPw = document.getElementById('inputPw').value;
-  const confirmPw = document.getElementById('confirmPw').value;
-  const pwPattern = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/;
-  const pwMsg = document.querySelector('#pwMsg');
-  const idMsg = document.querySelector('#idMsg');
-  const nameMsg = document.querySelector('#nameMsg');
-  if (inputId === '') {
-    idMsg.textContent = '아이디를 입력해주세요';
-  } else if (inputPw === '') {
-    pwMsg.textContent = '비밀번호를 입력해주세요';
-  } else if (inputName === '') {
-    nameMsg.textContent = '닉네임을 입력해주세요';
+const inputId = document.getElementById('inputId').value;
+const inputName = document.getElementById('inputName').value;
+const inputPw = document.getElementById('inputPw').value;
+const confirmPw = document.getElementById('confirmPw').value;
+const pwPattern = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/;
+const pwMsg = document.querySelector('#pwMsg');
+const idMsg = document.querySelector('#idMsg');
+const nameMsg = document.querySelector('#nameMsg');
+
+const overlapId = async () => {
+  const res = await axios({
+    url: '/user/register',
+    method: 'POST',
+    data: {
+      user_id: inputId,
+    },
+  });
+
+  if (res.data.result) {
+    idMsg.textContent = '사용 가능한 아이디입니다.';
   } else {
-    if (!pwPattern.test(inputPw)) {
+    idMsg.textContent = '중복된 아이디입니다.';
+  }
+};
+
+const overlapPw = async () => {
+  const res = await axios({
+    url: '/user/register',
+    method: 'POST',
+    data: {
+      user_name: inputName,
+    },
+  });
+
+  if (res.data.result) {
+    nameMsg.textContent = '사용 가능한 닉네임입니다.';
+  } else {
+    nameMsg.textContent = '중복된 닉네임입니다.';
+  }
+};
+
+const doRegister = async () => {
+  try {
+    const res = await axios({
+      url: '/user/register',
+      method: 'POST',
+      data: {
+        user_id: inputId,
+        password: inputPw,
+        user_name: inputName,
+      },
+    });
+
+    if (inputPw === confirmPw || !pwPattern.test(inputPw)) {
       pwMsg.textContent =
         '비밀번호는 8자 이상 영문 + 숫자를 혼합하여야 합니다.';
-    } else if (inputPw !== confirmPw) {
-      pwMsg.textContent = '비밀번호가 일치하지 않습니다.';
     } else {
-      // db로 보내는 코드 필요
-      alert('회원가입 성공!');
-      document.location.href = '/login';
+      pwMsg.textContent = '비밀번호가 일치하지 않습니다.';
     }
+  } catch (error) {
+    console.log(error);
   }
 };
