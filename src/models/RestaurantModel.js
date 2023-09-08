2ebf58b411d2ae460b1188bb9792cecabc18f9c2
kdm111 <pjsu94@naver.com>
2023-09-08 18:52:20 +0900

    feat : 삭제된 restaurant 컨트롤러, 모델, 라우터 등록

---
+++ src/models/RestaurantModel.js
@@ -0,0 +1,38 @@
+const RestaurantModel = (sequelize, Datatypes) => {
+  const Restaurant = sequelize.define("Restaurant", {
+    restaurant_id : {
+      type : Datatypes.INTEGER,
+      primaryKey : true,
+      allowNull : false,
+      autoIncrement : true
+    },
+    restaurant_name : {
+      type : Datatypes.STRING(256),
+      allowNull : false,
+    },
+    restaurant_likes : {
+      type : Datatypes.INTEGER,
+      default : 0,
+    },
+    restaurant_phone : {
+      type : Datatypes.STRING(256),
+    },
+    restaurant_address: {
+      type : Datatypes.STRING(256),
+    },
+    restaurant_kakao : {
+      type : Datatypes.STRING(1024),
+    },
+    restaurant_naver : {
+      type : Datatypes.STRING(1024)
+    }
+  }, {
+    tableName : "Restaurant",
+    freezeTableName : true,
+    charset : "utf8",
+    collate : "utf8_general_ci",
+    timestamps : false
+  });
+  return Restaurant;
+};
+module.exports = RestaurantModel;
