e98431bb152ebeeae7772832b58c4b42e14932ca
jjsh03 <aberrant03@gmail.com>
2023-09-08 17:46:12 +0900

    #3 feat: 로그인 유효성 검사

---
+++ static/js/login.js
@@ -0,0 +1,24 @@
+const msg = document.querySelector('#loginMsg');
+const form = document.forms['login'];
+
+const doLogin = async event => {
+  //   event.preventDefault();
+  try {
+    const res = await axios({
+      url: '/user/login',
+      method: 'POST',
+      data: {
+        user_id: form.inputId.value,
+        user_password: form.inputPw.value,
+      },
+    });
+    if (res.data.result) {
+      document.location.href = '/';
+    } else {
+      loginMsg.textContent = `아이디 또는 비밀번호를 잘못 입력했습니다.`;
+      document.location.reload();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
