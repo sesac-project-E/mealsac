a619ebf2a1019273b076f4804ca26390d0df2c94
junsu Park <pjsu94@naver.com>
2023-09-11 00:00:54 +0900

    Feat : 찜 취소하기

--- src/controllers/likeRestaurantController.js
+++ src/controllers/likeRestaurantController.js
@@ -3,21 +3,29 @@ const {userModels, LikeRestaurant} = require("../models")
 exports.postLike = (req, res) => {
   const {user_id, restaurant_id} = req.body
   LikeRestaurant.create({
-   restaurant_id : restaurant_id,
-   id : user_id
+    id : user_id,
+    restaurant_id : restaurant_id
   })
   .then(() => {
     res.status(201).send()
   })
+  .catch(() => {
+    res.status(400).send("이미 삭제되었습니다.")
+  })
 }
 
-
 exports.deleteLike = (req, res) => {
-  const {userId, restaurantId} = req.body
+  const {user_id, restaurant_id} = req.body
   LikeRestaurant.destroy({
-    where : {user_id : userId, restaurant_id : restaurantId}
+    where : {
+      id : user_id, 
+      restaurant_id : restaurant_id
+    }
   })
   .then(() => {
     res.status(204).send()
+  })
+  .catch(() => {
+    res.status(400).send()
   })
 }
