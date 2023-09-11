1ea13b188afc53dd51f813895e4058ad8156a66e
22-JWL <jck1004ee@gmail.com>
2023-09-11 17:30:36 +0900

    fix:userapi

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -30,8 +30,8 @@ const controller = require('../controllers/userController.js');
 router.get('/', controller.index);
 router.get('/users', controller.getUsers);
 
-router.get('/user/register', controller.getRegister);
-router.post('/user/register', controller.postRegister);
+router.get('/register', controller.getRegister);
+router.post('/register', controller.postRegister);
 
 router.get('/login', controller.getLogin);
 router.post('/login', controller.postLogin);
