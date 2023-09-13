a1fe30d3da3ea43733d9eabd1b3aebc606d8d220
jjsh03 <aberrant03@gmail.com>
2023-09-13 15:57:27 +0900

    #52 feat: 마이페이지 찜목록 불러오기 및 UI

--- src/static/js/mypage.js
+++ src/static/js/mypage.js
@@ -1,20 +1,13 @@
-const restaurant = {
-  restaurantName: '장수식당',
-  restaurantStyle: '한식',
-  restaurantPhone: '0226331870',
-  restaurantAddress: '서울특별시 영등포구 문래동2가 2-0번지',
-  restaurantRate: '4.5',
-  restaurantComment: '15',
-};
-
 const likeList = () => {
   document.getElementById('myLike').classList.add('clicked');
   document.getElementById('myReview').classList.remove('clicked');
-  document.querySelector('.list').innerHTML = '<h1>찜 목록입니다~</h1>';
+  document.getElementById('myLikes').style.display = 'block';
+  document.getElementById('myReviews').style.display = 'none';
 };
 
 const reviewList = () => {
   document.getElementById('myReview').classList.add('clicked');
   document.getElementById('myLike').classList.remove('clicked');
-  document.querySelector('.list').innerHTML = '<h1>리뷰관리</h1>';
+  document.getElementById('myReviews').style.display = 'block';
+  document.getElementById('myLikes').style.display = 'none';
 };
