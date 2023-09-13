e3567d3955cacc2156be02ff8dc30ebeb3722d35
kdm111 <pjsu94@naver.com>
2023-09-13 22:09:25 +0900

    Doc : 레스토랑 종류에 따른 분류

--- src/routes/restaurantTypeRouter.js
+++ src/routes/restaurantTypeRouter.js
@@ -2,11 +2,48 @@ const express = require("express")
 const restaurantTypeRouter = express.Router()
 const restaurantTypeController = require("../controllers/restaurantTypeController.js")
 
+/**
+ * @swagger
+ * tags:
+ *   name: RestaurantType
+ */
 
-restaurantTypeRouter.get("/:type", restaurantTypeController.getIndex)
-// restaurantTypeRouter.get("/:type", restaurantTypeController.getTypeRestaurants)
-
-
-
+/**
+ * @swagger
+ * tags:
+ *   name: RestaurantType
+ * @swagger
+ * /api/restaurant_type/:type_eng:
+ *  get:
+ *   summary: 해당 타입 식당 데이터 가져오기
+ *   tags : [RestaurantType]
+ *   responses:
+ *     200:
+ *      description : 해당 타입에 해당하는 식당 데이터가 배열로 전송됩니다.
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: array
+ *            items:
+ *             properties: 
+ *              restaurant_id:
+ *                type : int
+ *                example : 1 
+ *              restaurant_name:
+ *                type : string
+ *                example : "장수식당"
+ *              likes_count:
+ *                type : int
+ *                example : '0'
+ *              reviews_count:
+ *                type : int
+ *                example : '0'
+ *              rating:
+ *                type : int
+ *                example : '0'
+ * 
+ * 
+ * */
+restaurantTypeRouter.get("/:type", restaurantTypeController.getTypeRestaurants)
 
 module.exports = restaurantTypeRouter;
