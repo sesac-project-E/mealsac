8e3848a466407698a00cdcd546d4c59f994a5849
Cho Gain <chogain05@gmail.com>
2023-09-14 19:20:09 +0900

    #63 feat: 후기 등록 시 화면에 바로 보이도록 수정

--- src/static/js/restaurantDetail.js
+++ src/static/js/restaurantDetail.js
@@ -1,4 +1,6 @@
 const reviewForm = document.getElementById('reviewForm');
+const renderReviews = document.querySelector('.renderReviews');
+console.log(document.cookie);
 
 document
   .getElementById('showReviewForm')
@@ -22,20 +24,14 @@ document.getElementById('submitbtn').addEventListener('click', async e => {
   e.preventDefault();
 
   const formData = new FormData(reviewForm);
-  // const restaurant_id = formData.get('restaurantId');
-  // const title = formData.get('title');
   const content = formData.get('content');
   const rating = formData.get('rating');
   const imageFiles = formData.getAll('image');
   const restaurant_id = 1;
 
-  if (!title || !content || !rating) {
-    alert('제목, 내용, 평점을 모두 입력해주세요.');
+  if (!content || !rating) {
+    alert('내용과 평점을 모두 입력해주세요.');
     return;
-  }
-
-  for (let [key, value] of formData.entries()) {
-    console.log(`${key}: ${value}`);
   }
 
   try {
@@ -51,9 +47,40 @@ document.getElementById('submitbtn').addEventListener('click', async e => {
     if (response.status === 201) {
       alert('성공적으로 리뷰를 등록했습니다.');
       reviewForm.style.display = 'none';
+
+      const newReviewData = response.data.data;
+      addNewReview(newReviewData);
     }
   } catch (error) {
     console.error('에러 정보:', error);
     alert('리뷰를 등록하는 동안 오류가 발생했습니다.');
   }
 });
+
+const addNewReview = reviewData => {
+  const newReviewElement = document.createElement('div');
+
+  const today = new Date();
+  const yyyy = today.getFullYear();
+  const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+  const dd = String(today.getDate()).padStart(2, '0');
+  const formattedDate = `${yyyy}.${mm}.${dd}`;
+
+  const review = reviewData.review;
+
+  newReviewElement.innerHTML = `
+    <p>Rating: ${review.rating}</p>
+    <p>${review.content}</p>
+    <p>작성일 : ${formattedDate}</p>
+    <p>추천 : ${review.is_useful === undefined ? 0 : review.is_useful}</p>
+  `;
+
+  (reviewData.reviewImages || []).forEach(image => {
+    const imgElement = document.createElement('img');
+    imgElement.src = image.image_url;
+    imgElement.alt = 'Review Image';
+    newReviewElement.appendChild(imgElement);
+  });
+
+  renderReviews.prepend(newReviewElement);
+};
