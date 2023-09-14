185bc4fa9e706939b98c088172804d675bd9ed0a
Cho Gain <chogain05@gmail.com>
2023-09-15 03:20:08 +0900

    #63 feat: 식당 상세페이지 찜, 리뷰, 리뷰추천, 지도 로직 구현

--- src/static/js/restaurantDetail.js
+++ src/static/js/restaurantDetail.js
@@ -1,7 +1,85 @@
+const mainElement = document.querySelector('main');
+const callButton = document.getElementById('callButton');
 const reviewForm = document.getElementById('reviewForm');
 const renderReviews = document.querySelector('.renderReviews');
-console.log(document.cookie);
 
+// 갤럭시 폴드를 위한 코드
+const checkWidth = () => {
+  const windowWidth = window.innerWidth;
+
+  if (windowWidth <= 285) {
+    mainElement.classList.remove('container');
+  } else {
+    mainElement.classList.add('container');
+  }
+};
+
+checkWidth();
+
+window.addEventListener('resize', checkWidth);
+
+// 찜
+document.querySelector('.heart').addEventListener('click', async e => {
+  const currentSrc = e.target.getAttribute('src');
+
+  if (currentSrc === '/static/img/heart.png') {
+    try {
+      const response = await axios({
+        method: 'POST',
+        url: `/api/like`,
+        data: { restaurant_id: restaurant.restaurant_id },
+      });
+
+      if (response.status === 201) {
+        alert('찜 목록에 추가되었습니다.');
+        e.target.setAttribute('src', '/static/img/heart-filled.png');
+      }
+    } catch (error) {
+      console.error('에러 정보:', error);
+      alert('찜 목록에 추가하는 것을 실패했습니다.');
+    }
+  } else if (currentSrc === '/static/img/heart-filled.png') {
+    try {
+      const response = await axios({
+        method: 'DELETE',
+        url: `/api/like`,
+        data: { restaurant_id: restaurant.restaurant_id },
+      });
+
+      if (response.status === 200) {
+        alert('찜 목록에서 삭제되었습니다.');
+        e.target.setAttribute('src', '/static/img/heart.png');
+      }
+    } catch (error) {
+      console.error('에러 정보:', error);
+      alert('찜 목록에서 삭제하는 것을 실패했습니다.');
+    }
+  }
+});
+
+// 전화 걸기 or 전화번호 복사
+callButton.addEventListener('click', function () {
+  const phoneNumber = restaurant.restaurant_phone;
+
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent,
+    )
+  ) {
+    window.location.href = `tel:${phoneNumber}`;
+  } else {
+    navigator.clipboard
+      .writeText(phoneNumber)
+      .then(function () {
+        alert(`전화번호 ${phoneNumber}이(가) 클립보드에 복사되었습니다.`);
+      })
+      .catch(function (err) {
+        alert('전화번호를 복사하는 데 실패했습니다.');
+      });
+  }
+});
+
+// 리뷰 작성 폼 토글
 document
   .getElementById('showReviewForm')
   .addEventListener('click', function () {
@@ -20,6 +98,7 @@ reviewForm.addEventListener('submit', function (event) {
   reviewForm.style.display = 'none';
 });
 
+// 리뷰 등록
 document.getElementById('submitbtn').addEventListener('click', async e => {
   e.preventDefault();
 
@@ -47,6 +126,7 @@ document.getElementById('submitbtn').addEventListener('click', async e => {
     if (response.status === 201) {
       alert('성공적으로 리뷰를 등록했습니다.');
       reviewForm.style.display = 'none';
+      reviewForm.reset();
 
       const newReviewData = response.data.data;
       addNewReview(newReviewData);
@@ -57,6 +137,7 @@ document.getElementById('submitbtn').addEventListener('click', async e => {
   }
 });
 
+// 리뷰 등록 후 등록한 리뷰 바로 보여지도록 하는 함수
 const addNewReview = reviewData => {
   const newReviewElement = document.createElement('div');
 
@@ -84,3 +165,151 @@ const addNewReview = reviewData => {
 
   renderReviews.prepend(newReviewElement);
 };
+
+// 지도
+let geocoder;
+let map;
+
+function initMap() {
+  console.log('initMap is called');
+  geocoder = new google.maps.Geocoder();
+  const latlng = new google.maps.LatLng(-34.397, 150.644);
+  const mapOptions = {
+    zoom: 17,
+    center: latlng,
+  };
+
+  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+  codeAddress();
+  console.log('initMap is called');
+}
+
+function codeAddress() {
+  const address = restaurant.restaurant_address;
+
+  geocoder.geocode({ address: address }, function (results, status) {
+    if (status == 'OK') {
+      map.setCenter(results[0].geometry.location);
+      const marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location,
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+// 리뷰 페이지네이션
+let currentPage = 1;
+const reviewsPerPage = 5;
+
+function renderReviewsPage(page) {
+  const start = (page - 1) * reviewsPerPage;
+  const end = page * reviewsPerPage;
+
+  const reviewsToRender = restaurant.Reviews.slice(start, end);
+  const reviewsElement = document.querySelector('.renderReviews');
+
+  reviewsElement.innerHTML = ''; // 리뷰 컨테이너 초기화
+
+  if (reviewsToRender.length === 0) {
+    reviewsElement.innerHTML = '<p>후기가 없습니다.</p>';
+    return;
+  }
+
+  reviewsToRender.forEach(review => {
+    const reviewHTML = `
+    <section class="reviewContainer" >
+      <div>
+       <p>작성자</p>
+       <p>작성일: ${new Date(review.updatedAt)
+         .toLocaleDateString()
+         .replace(/\.$/, '')}</p>
+      </div>
+      <div>
+        <p><img src="/static/img/star.png" alt="평점" class="starIcon"> ${
+          review.rating
+        }</p>
+        <p><img src="/static/img/like.png" alt="추천하기" class="thumbsIcon" id=${
+          review.review_id
+        }> ${review.is_useful === undefined ? 0 : review.is_useful}</p>
+      </div>
+      <p>${review.content}</p>
+    </section>
+    `;
+    reviewsElement.innerHTML += reviewHTML;
+  });
+}
+
+renderReviewsPage(currentPage);
+
+document.querySelector('#nextPage').addEventListener('click', () => {
+  if (currentPage * reviewsPerPage < restaurant.Reviews.length) {
+    currentPage++;
+    renderReviewsPage(currentPage);
+  } else {
+    alert('이전에 작성된 후기가 더이상 없습니다.');
+  }
+});
+
+document.querySelector('#prevPage').addEventListener('click', () => {
+  if (currentPage > 1) {
+    currentPage--;
+    renderReviewsPage(currentPage);
+  } else {
+    alert('이후에 작성된 후기가 더이상 없습니다.');
+  }
+});
+
+// 리뷰 추천
+document
+  .querySelector('.renderReviews')
+  .addEventListener('click', async function (e) {
+    const target = e.target;
+
+    if (target.classList.contains('thumbsIcon')) {
+      const review_id = target.id;
+      const currentSrc = target.getAttribute('src');
+
+      if (currentSrc === '/static/img/like.png') {
+        try {
+          const response = await axios({
+            method: 'POST',
+            url: `/api/review/usefulness/${review_id}`,
+          });
+
+          if (response.status === 200) {
+            alert('성공적으로 리뷰를 추천하셨습니다.');
+            e.target.setAttribute('src', '/static/img/likeFilled.png');
+          }
+        } catch (error) {
+          if (
+            error.response &&
+            error.response.data &&
+            error.response.data.message
+          ) {
+            alert(error.response.data.message);
+          } else {
+            console.error('에러 정보:', error);
+            alert('리뷰를 추천하는 것을 실패했습니다.');
+          }
+        }
+      } else if (currentSrc === '/static/img/likeFilled.png') {
+        try {
+          const response = await axios({
+            method: 'DELETE',
+            url: `/api/review/usefulness/${review_id}`,
+          });
+
+          if (response.status === 200) {
+            alert('리뷰 추천을 취소하였습니다.');
+            e.target.setAttribute('src', '/static/img/like.png');
+          }
+        } catch (error) {
+          console.error('에러 정보:', error);
+          alert('리뷰 추천 취소를 실패하였습니다.');
+        }
+      }
+    }
+  });
