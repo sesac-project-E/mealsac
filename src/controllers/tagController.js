25008e184da9a4a22fb7285db58bbd9b10e98b22
kdm111 <pjsu94@naver.com>
2023-09-15 14:05:44 +0900

    Feat/#104 : 태그로 식당 찾기

--- src/controllers/tagController.js
+++ src/controllers/tagController.js
@@ -1,5 +1,5 @@
-const { Tag } = require("../models")
-
+const { Tag, Restaurant, TagRestaurant } = require("../models")
+const {Op} = require('sequelize')
 
 exports.getAllTags = (req, res) => {
   Tag.findAll()
@@ -8,9 +8,45 @@ exports.getAllTags = (req, res) => {
   })
 }
 
+exports.getRestaurantsByTag = async (req, res) => {
+  const tags = req.query
+  const tagIds = []
+  for (let [k, v] of Object.entries(tags)) {
+    try {
+      await Tag.findOne({
+        where : {tag_name_eng : v}
+      })
+      .then((response) => {
+        const id = response.dataValues.tag_id ? response.dataValues.tag_id : -1
+        tagIds.push({"tag_id" : id})
+      })
+    } catch (error) {
+      res.redirect("/badpage")
+      return ;
+    }
+  }
+  Restaurant.findAll({
+    attributes : ["restaurant_id", "restaurant_name", "likes_count", "reviews_count", "rating"],
+    include: [{
+        model : Tag,
+        where : {
+          [Op.or] : [...tagIds]
+        },
+      },
+    ],
+  })
+  .then((response) => {
+    response = response.filter((item) => {
+      return item.Tags.length == tagIds.length
+    })
+    res.send(response)
+  })
+  .catch(() => {
+    res.status(500).send("알 수 없는 에러")
+  })
+}
 // exports.getTagRestaurant = (req, res) => {
 //   const {tag} = req.params
-
 //   tagModels.findOne({
 //     where : {tag_name : tag}
 //   })
