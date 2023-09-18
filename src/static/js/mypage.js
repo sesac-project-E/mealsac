3ca4907606c8da59bb4839f04233adcf3eb12cb6
jjsh03 <aberrant03@gmail.com>
2023-09-18 10:42:00 +0900

    feat: mypage UI-글목록 추가

--- src/static/js/mypage.js
+++ src/static/js/mypage.js
@@ -1,15 +1,27 @@
 const likeList = () => {
   document.getElementById('myLike').classList.add('clicked');
   document.getElementById('myReview').classList.remove('clicked');
+  document.getElementById('myPost').classList.remove('clicked');
   document.getElementById('myLikes').style.display = 'block';
   document.getElementById('myReviews').style.display = 'none';
+  document.getElementById('myPosts').style.display = 'none';
 };
 
 const reviewList = () => {
   document.getElementById('myReview').classList.add('clicked');
   document.getElementById('myLike').classList.remove('clicked');
+  document.getElementById('myPost').classList.remove('clicked');
   document.getElementById('myReviews').style.display = 'block';
   document.getElementById('myLikes').style.display = 'none';
+  document.getElementById('myPosts').style.display = 'none';
+};
+const postList = () => {
+  document.getElementById('myPost').classList.add('clicked');
+  document.getElementById('myReview').classList.remove('clicked');
+  document.getElementById('myLike').classList.remove('clicked');
+  document.getElementById('myReviews').style.display = 'block';
+  document.getElementById('myLikes').style.display = 'none';
+  document.getElementById('myPosts').style.display = 'none';
 };
 
 const heartElements = document.querySelectorAll('.heart');
