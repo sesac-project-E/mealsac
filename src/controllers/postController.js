1edceef6f39b59d88f42bbd3f72a8569bfb7d37b
Cho Gain <chogain05@gmail.com>
2023-10-09 13:09:46 +0900

    #235 fix: 로그인하지 않은 경우 게시글 상세페이지 진입 시 userInfo is not defined 에러 발생으로 해당 컨트롤러에서 로그인 유무 확인 시 로그인 하지 않았다면 -1로 설정해 false를 반환하도록 수정

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -4,7 +4,7 @@ const { User, Post, PostImage, Comment } = require('../models');
 
 //post_id 값으로 특정 게시물 조회
 exports.getPost = async (req, res) => {
-  const userInfo = req.session ? req.session.userInfo : null;
+  const userInfo = req.session ? req.session.userInfo : -1;
   // [after]
   const { post_id } = req.params;
   const result = await Post.findOne({
